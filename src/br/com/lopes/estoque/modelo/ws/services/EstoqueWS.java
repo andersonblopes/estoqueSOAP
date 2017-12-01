c8ab573498e2fec57edb86614277f94fbb41d045
Anderson Lopes <anderson.lopes.2010@gmail.com>
2017-11-30 22:06:48 -0300

    Custom service

--- src/br/com/lopes/estoque/modelo/ws/services/EstoqueWS.java
+++ src/br/com/lopes/estoque/modelo/ws/services/EstoqueWS.java
@@ -3,9 +3,12 @@ package br.com.lopes.estoque.modelo.ws.services;
 import java.util.List;
 
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
 
+import br.com.lopes.estoque.modelo.item.Filtro;
+import br.com.lopes.estoque.modelo.item.Filtros;
 import br.com.lopes.estoque.modelo.item.Item;
 import br.com.lopes.estoque.modelo.item.ItemDao;
 import br.com.lopes.estoque.modelo.item.ListaItens;
@@ -17,11 +20,14 @@ public class EstoqueWS {
 
 	@WebMethod(operationName = "allItens")
 	@WebResult(name = "itens")
-	public ListaItens getItens() {
+	public ListaItens getItens(@WebParam(name = "filtros") Filtros filtros) {
+
 		System.out.println("Calling getItens...");
 
-		List<Item> lista = dao.todosItens();
-		return new ListaItens(lista);
+		List<Filtro> lista = filtros.getLista();
+
+		List<Item> itensResultado = dao.todosItens(lista);
+		return new ListaItens(itensResultado);
 	}
 
 }
